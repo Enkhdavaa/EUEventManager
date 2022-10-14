27fd07e29a857f401d8cfc3691db0f8fe25a980c
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2022-10-15 00:45:10 +0200

    added components and containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1 className="App-header">       
-          Learn React
-      </h1>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
