a0265041e1310c0768076a53e8d0bac628b65059
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2022-10-17 22:32:05 +0200

    added icons for navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { RiMenu3Line,  RiCloseLine} from 'react-icons/ri';
 import { VscCompass } from 'react-icons/vsc';
+import { AiOutlineShopping } from 'react-icons/ai';
+import { SlPeople } from 'react-icons/sl';
 import logo from '../../assets/ai.png';
 import './navbar.css';
 
 const Menu = () => {
     return (
         <>
-            <p> <VscCompass color="#000" size={27} /> </p>
+            <p> <VscCompass color="#000" size={40} /> Explore </p>
+            <p> <AiOutlineShopping color="#000" size={40} /> Shop </p>
+            <p> <SlPeople color="#000" size={40} /> Instructors </p>
         </>
     )
 }
@@ -22,14 +26,14 @@ const Navbar = () => {
                 <div className="gpt3__navbar-links_logo">
                     <img src={logo} alt="logo" />
                 </div>
-                <div className="gpt3__navbar-links_container">
-                    <Menu />
-                </div>
+            </div>
+            <div className="gpt3__navbar-links_container">
+                <Menu />
             </div>
             <div className="gpt3__navbar-sign">
-                <p> Sign in</p>
-                <button type="button"> Sign up </button>
+                <p> SIGN IN </p>
             </div>
+            {/* TODO: Please note that the below code is not usable enough */}
             <div className="gpt3__navbar-menu">
                 {toggleMenu
                     ? <RiCloseLine color="#000" size={27} onClick={() => setToggleMenu(false)} />
