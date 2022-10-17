fe1860109075e51c016eb5460d12410ed84ccfd6
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2022-10-17 21:16:00 +0200

    changed icon

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
 import { RiMenu3Line,  RiCloseLine} from 'react-icons/ri';
-import logo from '../../assets/logo.svg';
+import { VscCompass } from 'react-icons/vsc';
+import logo from '../../assets/ai.png';
 import './navbar.css';
 
 const Menu = () => {
     return (
         <>
-            <p><a href="#blog">Amita</a></p>
-            
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+            <p> <VscCompass color="#000" size={27} /> </p>
         </>
     )
 }
