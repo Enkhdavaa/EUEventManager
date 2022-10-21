5d56dae597ea9637a2ffe96c51d651fc7f7383b6
Enkhdavaa Batlkhagva <enkhdavaabatlkhagva@Enkhdavaas-MacBook-Pro.local>
2022-10-21 23:29:24 +0200

    added header contents

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
     return (
         <div className="amimoves__header">
             <img src={headerBackground} />
+            <div className="amimoves__header-onPicText">
+                 <h1> 50% OFF FOR LIFE </h1>
+                 <h2> SIgn up to lock in a monthly or annual membership at half off FOREVER. </h2>
+                 <button> Save Now </button>
+                 <p> Send a Gift Card </p>
+            </div>
         </div>
     )
 }
